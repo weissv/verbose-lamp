32309ecc2e0c242ff99f80b03252d973a85802cf
weissv <vveiss.vi@gmail.com>
2025-04-10 19:06:06 +0500

    Add files via upload

---
+++ backend/routes/subscribeRoute.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const Subscriber = require("../models/Subscriber");
+
+// @route POST /api/subscribe
+// @desc Handle newsletter subscription
+// @access Public
+router.post("/subscribe", async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+
+  try {
+    // Check if the email is already subscribed
+    let subscriber = await Subscriber.findOne({ email });
+
+    if (subscriber) {
+      return res.status(400).json({ message: "email is already subscribed" });
+    }
+
+    //  Create a new subscriber
+    subscriber = new Subscriber({ email });
+    await subscriber.save();
+
+    res
+      .status(201)
+      .json({ message: "Successfully subscribed to the newsletter!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+module.exports = router;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+// @route POST /api/users/register
+// @desc Register a new user
+// @access Public
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // Registration logic
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    user = new User({ name, email, password });
+    await user.save();
+
+    // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // Sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // Send the user and token in response
+        res.status(201).json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route POST /api/users/login
+// @desc Authenticate user
+// @access Public
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // Sign and return the token along with user data
+    console.log(process.env.JWT_SECRET)
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "100000000000000000h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // Send the user and token in response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route GET /api/users/profile
+// @desc Get logged-in user's profile (Protected Route)
+// @access Private
+router.get("/profile", protect, async (req, res) => {
+   res.json(req.user);
+ });
+
+
+module.exports = router;
