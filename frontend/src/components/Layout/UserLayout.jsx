79fd84b6efd3a0ace50c4a890c4f8992a2d2e9e7
weissv <vveiss.vi@gmail.com>
2025-04-10 18:45:34 +0500

    Add files via upload

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Common/Footer";
+import Header from "../Common/Header";
+
+const UserLayout = () => {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+      {/* Main content */}
+      <main>
+        <Outlet />
+      </main>
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+};
+export default UserLayout;
